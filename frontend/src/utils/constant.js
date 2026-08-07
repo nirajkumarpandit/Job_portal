385276f134e8156d12c35930daabaf7003bb3010
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2026-08-07 10:01:56 +0530

    deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="job-portal-jg73.vercel.app/api/user"
-export const JOB_API_END_POINT="job-portal-jg73.vercel.app/api/job"
-export const APPLICATION_API_END_POINT="job-portal-jg73.vercel.app/api/application"
-export const COMPANY_API_END_POINT="job-portal-jg73.vercel.app/api/company"
+export const USER_API_END_POINT="https://job-portal-jg73.vercel.app/api/user"
+export const JOB_API_END_POINT="https://job-portal-jg73.vercel.app/job"
+export const APPLICATION_API_END_POINT="https://job-portal-jg73.vercel.app/application"
+export const COMPANY_API_END_POINT="https://job-portal-jg73.vercel.app/company"
