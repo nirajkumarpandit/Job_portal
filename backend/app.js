40d26360b0db22983cf43302a07c47d427e11779
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2026-08-08 11:56:24 +0530

    fix bug

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption={
-    origin: 'job-portal-seven-chi-55.vercel.app',
+    origin: 'http://localhost:5173',
     credentials:true
 }
 
